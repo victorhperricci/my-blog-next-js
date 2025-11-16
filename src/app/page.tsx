a6ff1f13b33ad3b5a7ef5d06a33aa3560b8e83b0
Victor <victor.perricci@gmail.com>
2025-11-16 14:12:14 -0300

    Add global styles, layout, and homepage components; configure path mapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,6 +33,7 @@ export default function Home() {
             </a>{" "}
             center.
           </p>
+
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
